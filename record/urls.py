655a55588b355504a289431689a28725dc0636d8
David Okeke <iamdavonokeke@gmail.com>
2024-10-13 02:06:33 +0100

    fixing record/all url

--- record/urls.py
+++ record/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.create, name='create_record'),
-    path('all/', views.records, name='records' ),
+    path('all', views.records, name='records' ),
     path('<int:id>', views.record, name='record'),
     path('<int:id>/edit', views.edit_record, name='edit_record'),
     path('<int:id>/delete', views.delete_record, name='delete_record'),
